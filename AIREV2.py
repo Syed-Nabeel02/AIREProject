649d43f4058c5936905b4584a91826839bb63e2d
Dongwon Lee <42081181+MapleLiberty@users.noreply.github.com>
2022-09-28 11:55:18 -0400

    Add files via upload

--- AIREV2.py
+++ AIREV2.py
@@ -9,9 +9,27 @@ from difflib import SequenceMatcher
 
 """
     Fill in the report for us
+
+    open_home_menu() -> None
+    generate_pre_agp_0_report() -> None
+    get_assessment_data() -> list
+    get_similarity(rubric_text: str, rationale_text: str) -> float
+    get_rubric_descriptions() -> list
+    get_today_date_time() -> datetime
+    get_risk_level(risk_score: int) -> str
+    get_attribute_score(attribute_level: str) -> int
+    fill_in_template(initiative_name: str, attribute_levels: list, risk_score: int, rationales: list, corporate_or_cluster: str) -> None
+    check_file_exist(file_name: str) -> None
 """
 # this  function welcomes the user to the program
-def open_home_menu():
+def open_home_menu() -> None:
+    print("""
+    _______
+   /      /,
+  / AIRE //
+ /__V_2_//
+(______(/
+    """)
     print("WELCOME TO OUR ARCHITECTURE INTAKE REVIEW ENGINE BOT!")
     input("Press enter to continue :)")
 
@@ -19,6 +37,15 @@ def open_home_menu():
     answer = 0
 
     while invalid:
+        print("""
+    __...--~~~~~-._   _.-~~~~~--...__
+    //  1.Pre AGP 0  `V'   3.Exit      \\ 
+   //  2.AGP 0        |                 \\ 
+  //__...--~~~~~~-._  |  _.-~~~~~~--...__\\ 
+ //__.....----~~~~._\ | /_.~~~~----.....__\\
+====================\\|//====================
+                dwb `---`
+""")
         print("Please select which task you'd like to perform:\n")
         print("1. Pre AGP 0 Assessment: Use I&IT Decision Matrix to better assess a project item")
         print("2. AGP 0 Assessment: Ensure all mandatory files are submitted by Project group")
@@ -28,8 +55,7 @@ def open_home_menu():
             invalid = False
 
     if int(answer) == 1:
-        initiative_name = input("What is the initiative name? (any name):")
-        get_pre_AGP_0_report(initiative_name)
+        generate_pre_agp_0_report()
     elif int(answer) == 2:
         print("--- In Development ---")
         input("Press enter to exit")
@@ -39,7 +65,23 @@ def open_home_menu():
 
     return
 
-def get_pre_AGP_0_report(initiative_name: str) -> int:
+def generate_pre_agp_0_report() -> None:
+
+    initiative_name = input("What is the initiative name? (any name):")
+    
+    assessment_data = get_assessment_data()
+
+    attribute_levels = assessment_data[0]
+    risk_score = assessment_data[1]
+    rationales = assessment_data[2]
+    corporate_or_cluster = assessment_data[3]
+
+    fill_in_template(initiative_name, attribute_levels, risk_score, rationales, corporate_or_cluster)
+
+def get_assessment_data() -> list:
+    """
+    Get attribute_levels, risk_score, rationales, and corporate_or_cluster from the assessment file
+    """
     filename = input("Please enter the name of the assessment file (userinput.xlsx):")
 
     base_dir = Path(__file__).parent
@@ -53,17 +95,25 @@ def get_pre_AGP_0_report(initiative_name: str) -> int:
     rationales = [sheet['C10'].value, sheet['C11'].value, sheet['C12'].value, sheet['C13'].value, sheet['C14'].value]
     corporate_or_cluster = sheet['D22'].value
 
-    fill_in_template(attribute_levels, risk_score, rationales, initiative_name, corporate_or_cluster)
+    assessment_data = [attribute_levels, risk_score, rationales, corporate_or_cluster]
 
-    return 0
+    return assessment_data
 
-def get_similarity(rubric_text :str, input_text: str) -> float:
-    return SequenceMatcher(None, rubric_text, input_text).ratio()
+def get_similarity(rubric_text: str, rationale_text: str) -> float:
+    """
+    Get the text similarity between the rubric text and input text
+    """
+    return SequenceMatcher(None, rubric_text, rationale_text).ratio()
 
 
 def get_rubric_descriptions() -> list:
     """
-    Load data from the rubric "IIT-EA-Decision-Matrix.xlsx"
+    Get rubric description 0~12 from "IIT-EA-Decision-Matrix.xlsx" excel file
+    business_scopes: 0,1,2
+    it_solution: 3,4,5
+    technology_upgrade: 6,7,8
+    info_requirements: 9,10,11
+    info_sensitivy: 12,13,14
     """
     base_dir = Path(__file__).parent
     # word_doc = base_dir / "Architecture Intake Review Engine Report Draft.docx"
@@ -85,8 +135,10 @@ def get_today_date_time() -> datetime:
     date_time = today.strftime("%m/%d/%Y, %H:%M:%S")
     return date_time
 
-
 def get_risk_level(risk_score: int) -> str:
+    """
+    risk_score -> risk_level: Low - 7 - Medium - 11 - High 
+    """
     if risk_score < 7:
         risk_level = 'Low'
     elif 7 <= risk_score < 11:
@@ -95,8 +147,11 @@ def get_risk_level(risk_score: int) -> str:
         risk_level = 'High'
     return risk_level
 
-
 def get_attribute_score(attribute_level: str) -> int:
+    """
+    attribute_level -> attribute_score : Low -> 0, Medium -> 1, High -> 2
+    attributes: business_scopes, it_solution, technology_upgrade, info_requirements, info_sensitivy
+    """
     if attribute_level == 'Low':
         return 0
     elif attribute_level == 'Medium':
@@ -104,21 +159,27 @@ def get_attribute_score(attribute_level: str) -> int:
     else:
         return 2
 
+def check_file_exist(file_name: str) -> None:
+    if os.path.exists(file_name):
+        print(file_name, ' file exists')
+    else:
+        print(file_name, ' file does not exist')
+
 # this function should take in the array of rubric descriptions and user input arrays
 # based on the arrays, an appropriate set of results and conclusion should be reached
 # report should open automatically (probably remind the users to save the generated report)
-def fill_in_template(attribute_levels: list, risk_score: int, rationales: list, initiative_name: str, corporate_or_cluster: str) -> int:
+def fill_in_template(initiative_name: str, attribute_levels: list, risk_score: int, rationales: list, corporate_or_cluster: str) -> None:
 
     base_dir = Path(__file__).parent
-    word_doc = base_dir / "Architecture Intake Review Engine Report Draft.docx"
     output_dir = base_dir / "Output"
     output_dir.mkdir(exist_ok=True)
-
+    word_doc = base_dir / "Architecture Intake Review Engine Report Draft.docx"
     doc = DocxTemplate(word_doc)
     if risk_score < 7:
         gov = 'does not'
     else:
         gov = 'does'
+
     rubric_descriptions = get_rubric_descriptions() 
     business_scopes_rubrics = [rubric_descriptions[0], rubric_descriptions[1], rubric_descriptions[2]]
     it_solution_rubrics = [rubric_descriptions[3], rubric_descriptions[4], rubric_descriptions[5]]
@@ -154,10 +215,7 @@ def fill_in_template(attribute_levels: list, risk_score: int, rationales: list, 
                's4': str(round((get_similarity(info_sensitivy_rubrics[get_attribute_score(attribute_levels[4])], rationales[4]) * 100), 2)) + '%',
                }
 
-    if os.path.exists('demo1.docx'):
-        print('True')
-    else:
-        print('False')
+    check_file_exist('demo1.docx')
 
     doc.render(context)
 
@@ -166,12 +224,26 @@ def fill_in_template(attribute_levels: list, risk_score: int, rationales: list, 
 
     os.system("start " + str(output_path))
 
-    return 0
-
-
 # # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     open_home_menu()
 
     print("----------------------------------------")
+    print("""
+        _________   _________
+   ____/  AIRE   \ /  Report \____
+ /| ------------- |  ------------ |\\
+||| ------------- | ------------- |||
+||| ------------- | ------------- |||
+||| ------- ----- | ------------- |||
+||| ------------- | ------------- |||
+||| ------------- | ------------- |||
+|||  ------------ | ----------    |||
+||| ------------- |  ------------ |||
+||| ------------- | ------------- |||
+||| ------------- | ------ -----  |||
+||| ------------  | ------------- |||
+|||_____________  |  _____________|||
+L/_____/--------\\_//W-------\_____\J
+    """)
     print("File generation completed. Check the OUTPUT folder.")
