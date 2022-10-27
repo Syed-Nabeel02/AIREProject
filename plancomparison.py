0ab7e5a0605a991e3fe2b51cbd480103588ec934
Dongwon Lee <42081181+MapleLiberty@users.noreply.github.com>
2022-10-27 10:30:13 -0400

    Add files via upload

--- plancomparison.py
+++ plancomparison.py
@@ -19,19 +19,24 @@ def compare_opertional_plan_files(old_operationa_plan_directory, new_operationa_
         print("There are changes.")
 
     wb1 = openpyxl.load_workbook(excel_1_path)
-    sheet1 = wb1.active 
     wb2 = openpyxl.load_workbook(excel_2_path)
-    sheet2 = wb2.active 
 
     excel_changes_path = base_dir / "Operational_Plan_changes.xlsx"
     shutil.copyfile(excel_1_path, excel_changes_path)
     wb = openpyxl.load_workbook(excel_changes_path)
-    sheet = wb.active 
-    for row in range(1, sheet.max_row):
-        for col in range(1, sheet.max_column):
-            if sheet1.cell(row, col).value != sheet2.cell(row, col).value:
-                sheet.cell(row, col).value = '{} --> {}'.format(sheet1.cell(row, col).value, sheet2.cell(row, col).value)
-                sheet.cell(row, col).fill = PatternFill(start_color="A8F3FF", fill_type = "solid")
+
+    # sheet = wb.active 
+    sheet_names = ["RUN", "GROW", "TRANSFORM", "RUN-CLUSTER SERVICES", "INTAKE", "STRATEGY IMPLEMENTATION - SDWG", "Definitions"]
+    for sheet_name in sheet_names:
+        sheet = wb[sheet_name]
+        sheet1 = wb1[sheet_name]
+        sheet2 = wb2[sheet_name]
+        for row in range(1, sheet.max_row):
+            for col in range(1, sheet.max_column):
+                if sheet1.cell(row, col).value != sheet2.cell(row, col).value:
+                    sheet.cell(row, col).value = '{} --> {}'.format(sheet1.cell(row, col).value, sheet2.cell(row, col).value)
+                    sheet.cell(row, col).fill = PatternFill(start_color="A8F3FF", fill_type = "solid")
+        
     wb.save(excel_changes_path)
 
     output_dir = base_dir / "Operational_Plan_changes.xlsx"
